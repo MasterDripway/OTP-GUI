02493c776c3a0573671f5db1b753490bed3f743e
MasterDripway <manuelinegron@gmail.com>
2023-02-14 13:13:37 -0400

    .

--- lib/rust_wrap.py
+++ lib/rust_wrap.py
@@ -1,9 +1,11 @@
 import subprocess as sp
+import os
 
+NAUTILUS_HOME = os.environ.get('NAUTILUS_HOME', '~')
 
 def encode(input_name, output_name, key_name):
-    return sp.run(['../flash-otp/target/release/flash-otp', 'encode', input_name, output_name, key_name], capture_output=True)
+    return sp.run([f'{NAUTILUS_HOME}/flash-otp/target/release/flash-otp', 'encode', input_name, output_name, key_name], capture_output=True)
 
 def decode(input_name, output_name, key_name):
-    return sp.run(['../flash-otp/target/release/flash-otp', 'decode', input_name, output_name, key_name], capture_output=True)
+    return sp.run([f'{NAUTILUS_HOME}/flash-otp/target/release/flash-otp', 'decode', input_name, output_name, key_name], capture_output=True)
 
